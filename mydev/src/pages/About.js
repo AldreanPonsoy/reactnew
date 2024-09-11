52ef44a0563ad5071fc2a693336a9db39be1fc2c
AldreanPonsoy <Aldreanponsoy11@gmail.com>
2024-09-11 18:07:03 +0800

    about design

--- mydev/src/pages/About.js
+++ mydev/src/pages/About.js
@@ -1,12 +1,24 @@
+import React from 'react';
+import './stylepages/about.css';
+import logo from './stylepages/mylogo.png';
+
 export default function About() {
     return ( 
-        <div class="container">
-            <h1>Hi! Im Developer for this Webpage</h1>
+        <section class="home" id="home">
+            <div class="home-content">
+                <h3>Hi! Im Developer for this Webpage</h3>
+                <h1>Aldrean Ponsoy</h1>
+                <h3>Explore my<span class="text"></span></h3>
+                <button class="btn">Download CV</button>
+                <div class="social-media-icon">
+                    <a href="https://github.com/Leo211990" target="_blank"><i class='bx bxl-github' ></i></a>
+                </div>
+            </div>    
             <div>
-                <p>
-                    NOTE!!!
-                </p>
+                <div class="home-img">
+                    <img src={logo} alt="My Image"></img>
+                </div>
             </div>
-        </div>
+        </section>    
     )
 }
