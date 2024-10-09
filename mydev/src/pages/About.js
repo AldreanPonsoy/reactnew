0a0209fdc8febdcb038badd216f18a96cab3857c
AldreanPonsoy <Aldreanponsoy11@gmail.com>
2024-10-09 18:46:43 +0800

    oct 9 2024

--- mydev/src/pages/About.js
+++ mydev/src/pages/About.js
@@ -2,26 +2,44 @@ import React, { useEffect } from 'react';
 import './stylepages/about.css';
 import logo from './stylepages/mylogo.png';
 import { FaGithub } from 'react-icons/fa';
-
+import Typed from 'typed.js'; // Import Typed.js
 
 export default function About() {
+    useEffect(() => {
+        // Initialize Typed.js
+        const options = {
+            strings: ["Explore my website"],
+            typeSpeed: 90,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true,         
+        };
+        const typed = new Typed(".text", options);
+
+        // Cleanup on unmount
+        return () => {
+            typed.destroy();
+        };
+    }, []);
 
     return ( 
-        <section class="home" id="home">
-            <div class="home-content">
-                <h3>Hi! Im Developer for this Webpage</h3>
+        <section className="home" id="home">
+            <div className="home-content">
+                <h3>Hi! I'm Developer for this Webpage</h3>
                 <h1>Aldrean Ponsoy</h1>
-                <h3>Explore my<span class="text" ></span></h3>
-                <button class="btn" href="CV_PONSOY.PDF" download="CV_PONSOY.PDF">Download CV</button>
-                <div class="social-media-icon">
-                    <a href="https://github.com/AldreanPonsoy" target="_blank"><FaGithub size={30} color="#000" /></a>
+                <h3><span className="text"></span></h3>
+                <a className="btn" href="CV_PONSOY.PDF" download="CV_PONSOY.PDF">Download CV</a>
+                <div className="social-media-icon">
+                    <a href="https://github.com/AldreanPonsoy" target="_blank" rel="noopener noreferrer">
+                        <FaGithub size={30} color="#000" />
+                    </a>
                 </div>
             </div>    
             <div>
-                <div class="home-img">
-                    <img src={logo} alt="My Image"></img>
+                <div className="home-img">
+                    <img src={logo} alt="My Image" />
                 </div>
             </div>
         </section>    
-    )
+    );
 }
